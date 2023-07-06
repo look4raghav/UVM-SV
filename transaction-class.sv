91da7d11b9fc9b25e3424996d0cf8e9ad228f0ad
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-07-06 20:49:31 +0530

    Update transaction-class.sv

--- transaction-class.sv
+++ transaction-class.sv
@@ -13,3 +13,47 @@ class packet extends uvm_sequence_item;
     super.new(name);
   endfunction
 endclass
+
+module test;
+  initial 
+    begin
+      packet obj1;
+      obj1 = new();
+      obj1.randomize();
+      obj1.print();
+  end
+  initial 
+    begin
+      packet obj2;
+      packet obj3;
+      obj2=new();
+      obj3=new();
+      obj2.randomize();
+      obj2.print();
+    //copy method
+      obj3.copy(obj2);
+      obj3.print();
+     //matching case 
+      if(obj2.compare(obj3))
+        `uvm_info("","obj2 is matching with obj3",UVM_LOW)
+        else
+          `uvm_error("","obj2 is not matching with obj3")
+  end
+  initial begin
+    packet obj4;
+    packet obj5;
+    obj4=new();
+    obj5=new();
+    obj4.randomize();
+    obj5.randomize();
+    obj4.print();
+    obj5.print();
+    
+    //compare method
+    if(obj4.compare(obj5))
+      `uvm_info("","obj4 is matching with obj5",UVM_LOW)
+    else
+      `uvm_error("","obj4 is not matching with obj5")
+  end
+endmodule
+  
